4236a818ec3f7ff5f14439f15c34c597e1624287
diogonc <diogonc@gmail.com>
2015-06-18 12:16:50 -0400

    filtering by date

--- www/js/controllers/transactionListCtrl.js
+++ www/js/controllers/transactionListCtrl.js
@@ -1,8 +1,29 @@
-angular.module("finance").controller("TransactionListCtrl", function ($scope, $location, TransactionRepository) {
-	$scope.itens = TransactionRepository.getAll();
+angular.module("finance").controller("TransactionListCtrl", function ($scope, $stateParams, $location, TransactionRepository) {
+	$scope.itens = [];
 	$scope.newItem = newItem;
+	$scope.search = search;
 	
 	function newItem(){
 		$location.path('app/transactions/0');
 	};
+	
+	function search(item){
+		$location.path('app/transactions').search({year: item.year, month: item.month});
+	};
+	
+	$scope.$on('$ionicView.beforeEnter', function() {
+		
+		var hoje = new Date();
+		var year = hoje.getFullYear();
+		var month = hoje.getMonth() + 1;		
+		
+		if(typeof $stateParams.year !== 'undefined')
+		 	year = parseInt($stateParams.year);
+		if(typeof $stateParams.month !== 'undefined')
+		 	month = parseInt($stateParams.month);		
+		
+		$scope.item = {year: year , month: month};
+		
+		$scope.itens = TransactionRepository.getAll($scope.item.year, $scope.item.month);
+	});
 });
