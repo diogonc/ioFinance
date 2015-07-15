73132c58dfcf5038252e600a1f171474fa3c1419
diogonc <diogonc@gmail.com>
2015-07-15 12:21:26 -0400

    travis

--- www/js/controllers/balancePerMonthCtrl.js
+++ www/js/controllers/balancePerMonthCtrl.js
@@ -1,19 +1,23 @@
 angular.module("finance").controller("BalancePerMonthCtrl", function($scope, $stateParams, $location, TransactionRepository) {
   $scope.itens = [];
-  $scope.item = {months:3};
+  $scope.item = {
+    months: 1
+  };
   $scope.search = search;
 
-  function search(item){
-		$location.path('app/balance-per-month').search({months: item.months});
-	};
+  function search(item) {
+    $location.path('app/balance-per-month').search({
+      months: item.months
+    });
+  };
 
   $scope.$on('$ionicView.beforeEnter', function() {
     var report = new BalancePerMonthReport();
     var data = TransactionRepository.getAllTransactions();
 
-    var numberOfMonths = 3;
-    if(typeof $stateParams.months !== 'undefined')
-       numberOfMonths = parseInt($stateParams.months);
+    var numberOfMonths = 1;
+    if (typeof $stateParams.months !== 'undefined')
+      numberOfMonths = parseInt($stateParams.months);
 
     $scope.item.months = numberOfMonths;
     $scope.itens = report.GetBalancePerMonth(data, numberOfMonths);
